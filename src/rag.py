114cf7d352251686f7ecb1ba4b18c6802e3af0b2
Estifanos Lulseged <estlul2004@gmail.com>
2025-07-06 09:01:10 +0300

    Done Embedding, chunking and indexing

---
+++ src/rag.py
@@ -0,0 +1,18 @@
+import faiss
+import numpy as np
+import pickle
+from sentence_transformers import SentenceTransformer
+
+# Load index and metadata
+index = faiss.read_index('vector_store/faiss_index.index')
+with open('vector_store/chunks_with_embeddings.pkl') as f:
+    documents = pickle.load(f)
+
+# Load embedding model
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def retrieve_top_k_chunks(query, k=5):
+    query_vec = model.encode([query]).astype('float32')
+    distance, indices = index.search(query_vec, k)
+    return [documents[i] for i in indices[0]]
+
